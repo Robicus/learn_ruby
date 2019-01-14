ab28c4f022141e2ccec10b34fa479278ad179255
Robert Adams <athcure@gmail.com>
2019-01-13 21:12:14 -0700

    Finished 00 and 01.

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,13 @@
 #write your code here
+# (ftoc(32)).to eq(0)
+# (32°F − 32) × 5/9 = 0°C
+def ftoc(num)
+    (num - 32) * 5 / 9
+end
+
+def ctof(num)
+    # (0°C × 9/5) + 32 = 32°F
+    # expect(ctof(100)).to eq(212)
+    (num * 9 / 5) + 32
+end
+
